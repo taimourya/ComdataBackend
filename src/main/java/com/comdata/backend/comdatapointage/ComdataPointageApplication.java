47f3c53726c14c6f8128665f24a2e25e11df1585
taimourya <taimouriya@gmail.com>
2021-08-25 17:29:53 +0200

    First commit

---
+++ src/main/java/com/comdata/backend/comdatapointage/ComdataPointageApplication.java
@@ -0,0 +1,13 @@
+package com.comdata.backend.comdatapointage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComdataPointageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComdataPointageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comdata/backend/comdatapointage/ComdataPointageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comdata.backend.comdatapointage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComdataPointageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
